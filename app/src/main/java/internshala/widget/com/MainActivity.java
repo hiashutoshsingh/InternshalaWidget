9d6c970bd0b6a7b337dc7ae8b2a4df11e566e724
ashutoshkumarsingh <ashutoshsingh.0207@gmail.com>
2019-04-18 13:30:03 +0530

    adapter called

--- app/src/main/java/internshala/widget/com/MainActivity.java
+++ app/src/main/java/internshala/widget/com/MainActivity.java
@@ -2,6 +2,9 @@ package internshala.widget.com;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+
+        ItemData itemsData[] = { new ItemData("Internship-1",R.drawable.ic_launcher_foreground),
+                new ItemData("Internship-2",R.drawable.ic_launcher_foreground),
+                new ItemData("Internship-3",R.drawable.ic_launcher_foreground),
+                new ItemData("Internship-4",R.drawable.ic_launcher_foreground),
+                new ItemData("Internship-5",R.drawable.ic_launcher_foreground),
+                new ItemData("Internship-6",R.drawable.ic_launcher_foreground)};
+
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        MyAdapter mAdapter = new MyAdapter(itemsData);
+        recyclerView.setAdapter(mAdapter);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
     }
 }
